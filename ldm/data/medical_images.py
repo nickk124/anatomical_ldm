abf15be463e7f7f330921566b95aa4607b5a6d57
Nick Konz <nickkonz3@gmail.com>
2025-06-27 10:23:59 -0400

    sampling fix

--- ldm/data/medical_images.py
+++ ldm/data/medical_images.py
@@ -139,7 +139,7 @@ class CTOrganTest(MedicalImagesBase):
 class CTOrganSmallTrain(MedicalImagesBase):
     def __init__(self, **kwargs):
         super().__init__(
-            data_root="../../other_data/ct_organ/images/train_subset_100",
-            segmentation_root="../../other_data/ct_organ/masks/all/train_subset_100",
+            data_root="../../other_data/ct_organ/images/train_subset_1000",
+            segmentation_root="../../other_data/ct_organ/masks/all/train_subset_1000",
             n_labels=6,
             **kwargs)
